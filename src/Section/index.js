5b46a78d90bb94fb4f60efb321e2fe66d04846fe
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-02-17 10:35:44 +0100

    changegd div for header in Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,14 +2,14 @@ import "./style.css";
 
 const Section = ({title, type, buttons}) => (
     <section className="section">
-    <div className="section__header section__header--split ">
+    <header className="section__header section__header--split ">
         <div>
            {title}
         </div>
         <div className="section__hiddenButtons">
             {buttons}
         </div>
-    </div>
+    </header>
     <div className="section__body">
         {type}
     </div>
