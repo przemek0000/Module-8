ae0d164a1c9e3970dbaeb1f25d334ff95841928e
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-02-17 09:52:12 +0100

    fixed nomenclature BEM for Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
 const Form = () => (
-    <form className="section__form">
-        <input className="section__input" placeholder="Co jest do zrobienia?" autoFocus />
-        <button className="section__addNewTaskbutton">Dodaj zadanie</button>
+    <form className="form">
+        <input className="form__input" placeholder="Co jest do zrobienia?" autoFocus />
+        <button className="form__addNewTaskbutton">Dodaj zadanie</button>
     </form>
 );
 
